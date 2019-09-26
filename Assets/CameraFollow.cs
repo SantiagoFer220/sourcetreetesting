824c0060e01799af42c9ed493e44f0ae5dd7e962
SantiagoFer <49788541+SantiagoFer@users.noreply.github.com>
2019-09-26 16:45:04 -0400

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+public Transform target;
+
+Vector3 offsetVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       offsetVector = this.transform.position - target.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.LookAt(target);
+
+        // making the follow below better. Locks x and y pos of the camera.
+        Vector3 newpos = target.position + offsetVector;
+        newpos.y = this.transform.position.y;
+        newpos.z = this.transform.position.z;
+
+        //have camera interpolate between locations to improve feel
+        //this.transform.position = Vector3.Lerp(this.transform.position, newpos, 0.5f)
+        //this.transform.position = Vector3.MoveTowards(this.transform.position, newpos, 0.5f);
+
+
+        Vector3 moveVector = this.transform.position - newpos;
+        moveVector *= 0.25f;
+        newpos+= moveVector;
+        this.transform.position = newpos;
+
+        //looks at position but not rotation. follows all positions. 
+        //this.transform.position = target.position + offsetVector;
+
+    }
+}
